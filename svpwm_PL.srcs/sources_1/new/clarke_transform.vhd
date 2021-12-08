97d3985991edfc76fd67abe7a3947b05cf2b0749
GanManCan <mgannon1606@gmail.com>
2021-12-07 20:24:23 -0500

    Compiled, Not tested. Implemented Clarke_transform

--- svpwm_PL.srcs/sources_1/new/clarke_transform.vhd
+++ svpwm_PL.srcs/sources_1/new/clarke_transform.vhd
@@ -38,12 +38,13 @@ use IEEE.FIXED_PKG.ALL;
 entity clarke_transform is
 	GENERIC(
 		bits_res_high	: integer := 20;
-		bits_res_low 	: integer := -11; 
+		bits_res_low 	: integer := -11 
 	); 
 	
 	PORT( 
 	  clk 			: in STD_LOGIC;
 	  clarke_start 	: in STD_LOGIC; 
+	  reset_n		: in std_logic; 
 	  fp_y_a 		: in sfixed(bits_res_high downto bits_res_low);
 	  fp_y_b 		: in sfixed(bits_res_high downto bits_res_low);
 	  fp_y_c 		: in sfixed(bits_res_high downto bits_res_low); 
@@ -56,20 +57,23 @@ end clarke_transform;
 architecture logic of clarke_transform is
 
 	-- Create State Machine Type
-	type STATE_TYPE is (IDLE, MULT_Y_A, MULT_Y_B, DONE);
+	type STATE_TYPE is (IDLE, MULT_Y_ALPHA, MULT_Y_BETA, DONE);
 	signal state : STATE_TYPE; 
 	
 	-- Constant Declarations
-	constant int_mult_conter : integer range 0 to 255 := 0; 
+	CONSTANT int_mult_delay : integer range 0 to 7 := 2; 
 	
 	-- Constants for math
 	CONSTANT fp_2_3		: sfixed(1 downto bits_res_low) := to_sfixed(2.0/3.0, 1, bits_res_low);
 	CONSTANT fp_1_3		: sfixed(1 downto bits_res_low) := to_sfixed(1.0/3.0, 1, bits_res_low);
 	CONSTANT fp_2_sqrt3	: sfixed(1 downto bits_res_low) := to_sfixed(1.0/3.0, 1, bits_res_low);
 
+	-- signal declarations
+	signal int_mult_count : integer range 0 to 255 := 0; 
 	
 	-- fixed point signal declaration
-	fp_ya_min_yb	: sfixed(bits_res_high downto bits_res_low);
+	signal fp_y_a_hold, fp_y_b_hold, fp_y_c_hold : sfixed(bits_res_high downto bits_res_low); 
+	signal fp_yb_min_yc	: sfixed(bits_res_high downto bits_res_low);
 
 begin
 	process(clk,reset_n)
@@ -89,17 +93,53 @@ begin
 			
 				when IDLE =>
 					clarke_done <= '0'; 
-				
-				when MULT_Y_A =>
-					fp_ya_min_yb <= resize(fp_y_alpha - fp_y_beta,fp_ya_min_yb); 
+					fp_yb_min_yc <= (OTHERS => '0'); 
+					
+					if(clarke_start = '1') then
+						-- Lock in y_alpha and y_beta for calculations
+						fp_y_a_hold <= fp_y_a; 
+						fp_y_b_hold <= fp_y_b;
+						fp_y_c_hold <= fp_y_c; 
+						
+						-- Move to next state
+						state <= MULT_Y_ALPHA; 
+						
+					end if; --if(clark_starte = 1)
+				when MULT_Y_ALPHA =>
+					
+					-- Calculate subtraction used for this and next multiplication
+					fp_yb_min_yc <= resize(fp_y_b_hold - fp_y_c_hold, fp_yb_min_yc); 
+					
+					-- Calcualte y_alpha
+					fp_y_alpha <= resize(fp_2_3*fp_y_a_hold - fp_1_3*fp_yb_min_yc, fp_y_a_hold);
+					
+					-- Increment multiply counter and check for next state; 
+					int_mult_count <= int_mult_count + 1; 
+					if(int_mult_count >= int_mult_delay) then
+						int_mult_count <= 0;  
+						state <= MULT_Y_BETA;  
+					end if; -- end if(int_mult_count>- int_mult_delay); 
 					
 				
-				when MULT_Y_B =>
-					fp_ya_min_yb <= resize(fp_y_alpha - fp_y_beta, fp_ya_min_yb); 
+				when MULT_Y_BETA =>
+					
+							
+					-- Calcualte y_alpha
+					fp_y_beta <= resize(fp_2_sqrt3*fp_yb_min_yc, fp_y_b_hold);
+					
+					-- Increment multiply counter and check for next state; 
+					int_mult_count <= int_mult_count + 1; 
+					if(int_mult_count >= int_mult_delay) then
+						int_mult_count <= 0;  
+						state <= DONE;  
+					end if; -- end if(int_mult_count>- int_mult_delay); 
 				
 				when DONE =>
+					-- Raise clarke_done flag for one clock cyle 
+					--  and move to IDLE state
 					clarke_done <= '1'; 
-				
+					state <= IDLE; 
+					
 				when OTHERS => 
 					-- Invalid State
 					-- Stall state machine 
@@ -108,8 +148,6 @@ begin
 					fp_y_beta <= (OTHERS => '0'); 
 			
 			end case; -- case state
-		
 		end if; --if(reset_n=0)
-
-
+	end process; 
 end logic;
