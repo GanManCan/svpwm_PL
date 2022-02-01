b5c16980401261180d4bd069242c266da16675dd
GanManCan <mgannon1606@gmail.com>
2022-01-31 20:38:32 -0500

    Add intial firing_time_test

--- vunit_tests/run.py
+++ vunit_tests/run.py
@@ -15,15 +15,21 @@ TEST_PATH = ROOT / "vunit_tests" / "test_benches"
 print(TEST_PATH)
 
 # create VUnit instance
-VU = VUnit.from_argv(compile_builtins=False)
+VU = VUnit.from_argv()
 VU.enable_location_preprocessing()
 
 # create design library and add source files
 design_lib = VU.add_library("design_lib")
-design_lib.add_source_files([DUT_PATH / "*.vhd"])
+design_lib.add_source_files([DUT_PATH / "firing_time_generator.vhd"])
 
 # create testbench library
 testbench_lib = VU.add_library("testbench_lib")
 testbench_lib.add_source_files([TEST_PATH / "*.vhd"])
+
+#VU.set_sim_option("modelsim.vsim_flags", ["+notimingchecks"])
+#for tb in testbench_lib.get_test_benches():
+#  tb.set_sim_option("modelsim.init_file.gui", str(ROOT) + "/waves/" + tb.name + "_wave.do")
+
+VU.main()
 
 
---
+++ vunit_tests/vunit_out/preprocessed/testbench_lib/firing_time_generator_vunit_tb.vhd
@@ -0,0 +1,189 @@
+----------------------------------------------------------------------------
+--  % NAME        : firing_time_generator_tb_vunit %                                 --
+--  % PROJECT     : SVPWM_PL      %                                 --
+--  % VERSION     :                  %                                 --
+--  % CREATED_BY  : Matt Gannon      %                                 --
+--  % DATE_CREATED: 2021-01-23        %                                 --
+--                                                                        --
+--  DESCRIPTION:                                                          --
+--                                                                        --
+----------------------------------------------------------------------------
+--  REVISION HISTORY                                                      --
+--                                                                        --
+--  VERSION    DATE        SIGN      CHANGE DESCRIPTION                   --
+--  ------- ----------  ----------   ------------------                   --
+--                      --
+--                                                                        --
+----------------------------------------------------------------------------
+--  TODO LIST                                                             --
+--  Test reset
+--  Test Initialization 
+--  Test Do Nothin Until Receive Start Signal
+-- 	Test Outputs for correct values
+--     Test values for each sector 
+--  Test out-of-bounds values
+--  T                                         --
+----------------------------------------------------------------------------
+
+----------------------------------------------------------------------------
+-- CONFIGURATION DECLARATION.
+----------------------------------------------------------------------------
+
+----------------------------------------------------------------------------
+-- LIBRARY DECLARATION.
+----------------------------------------------------------------------------
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
+use IEEE.FIXED_PKG.ALL; 
+
+LIBRARY VUNIT_LIB;
+CONTEXT VUNIT_LIB.VUNIT_CONTEXT;
+
+LIBRARY DESIGN_LIB;
+--USE DESIGN_LIB.MOTOR_PKG.ALL;
+
+LIBRARY TESTBENCH_LIB;
+--USE TB_LIB.MOTOR_TB_PKG.ALL;
+
+library modelsim_lib;
+use modelsim_lib.util.all;
+
+
+
+
+entity firing_time_generator_vunit_tb is
+  generic(runner_cfg : string := runner_cfg_default);
+end entity firing_time_generator_vunit_tb;
+
+
+
+architecture tb of firing_time_generator_vunit_tb is
+
+  --------------------------------------------------------------------------
+  -- TYPES, RECORDS, INTERNAL SIGNALS, FSM, CONSTANTS DECLARATION.
+  --------------------------------------------------------------------------
+  -- CONSTANTS DECLARATION --
+
+  -- spy signals
+
+  -- DUT constants
+  constant sys_clk         : INTEGER := 50_000_000;  
+  constant pwm_freq        : INTEGER := 3_000;      
+  constant bits_resolution : INTEGER := 32;           
+  constant v_dc            : INTEGER := 200;
+
+  -- simulation constants :
+  constant c_clk_period : time := 20 ns;
+  constant temp_sl : std_logic := '1'; 
+
+  -- INTERNAL SIGNALS DECLARATION --
+  -- DUT interface
+  signal clk             : std_logic := '0';
+  signal reset_n         : std_logic := '0';
+  signal fire_time_start : std_logic := '0';
+  signal fp_v_alpha      : sfixed(20 downto -11);
+  signal fp_v_beta       : sfixed(20 downto -11);
+  signal fire_time_done  : std_logic := '0';
+  signal fire_u          : std_logic_vector(bits_resolution-1 downto 0);
+  signal fire_v          : std_logic_vector(bits_resolution-1 downto 0);
+  signal fire_w          : std_logic_vector(bits_resolution-1 downto 0); 
+
+  -- Simulation Signals --
+  signal test_fire       : std_logic_vector(bits_resolution-1 downto 0);
+
+  -- Spy Signal
+  --signal dut_path :string := "/firing_time_generator_vunit_tb/firing_time_generator_tb_inst/";
+  --signal spy_sim_sector : integer;
+
+  alias spy_sim_sector is
+    <<signal.firing_time_generator_tb_inst.sim_sector : integer >>;
+
+begin -- start of architecture -- 
+  -------------------------------------------------------------------------- 
+  -- DUT INSTANTIATION.
+  --------------------------------------------------------------------------
+  
+
+  firing_time_generator_tb_inst : entity DESIGN_LIB.firing_time_generator
+    generic map (
+      sys_clk         => sys_clk,
+      pwm_freq        => pwm_freq,
+      bits_resolution => bits_resolution,
+      v_dc            => v_dc
+    )
+    port map (
+      clk             => clk,
+      reset_n         => reset_n,
+      fire_time_start => fire_time_start,
+      fp_v_alpha      => fp_v_alpha,
+      fp_v_beta       => fp_v_beta,
+      fire_time_done  => fire_time_done,
+      fire_u          => fire_u,
+      fire_v          => fire_v,
+      fire_w          => fire_w
+    );  
+
+  --------------------------------------------------------------------------
+  -- CLOCK AND RESET.
+  --------------------------------------------------------------------------
+  clk   <= NOT clk after C_CLK_PERIOD / 2;
+  reset_n <= '1' after 5 * (C_CLK_PERIOD / 2);
+
+
+  --------------------------------------------------------------------------
+  --------------------------------------------------------------------------
+  -------------------------- spy_process PROCESS ---------------------------
+  --------------------------------------------------------------------------
+  --------------------------------------------------------------------------
+
+
+  --spy_process : process
+  --begin
+  --  init_signal_spy(dut_path & "sim_sector","spy_sim_sector", 1);
+  --  wait;
+  --end process spy_process;
+
+
+  --------------------------------------------------------------------------
+  --------------------------------------------------------------------------
+  -------------------------- TEST_RUNNER PROCESS ---------------------------
+  --------------------------------------------------------------------------
+  --------------------------------------------------------------------------
+
+  test_runner : process
+  begin
+    -- setup VUnit
+    test_runner_setup(runner, runner_cfg);
+    -- enable logging for passing check
+    show(get_logger(default_checker), display_handler, pass); 
+
+    test_cases_loop : while test_suite loop
+      ----------------------------------------------------------------------
+      -- TEST CASE DESCRIPTION:
+      -- check output when design all switches are OFF.
+      -- Expected Result:
+        --  fire_u/v/w = '111..111'
+      ----------------------------------------------------------------------
+      if run("check_reset_values") then
+        info("--------------------------------------------------------------------------------", line_num => 169, file_name => "firing_time_generator_vunit_tb.vhd");
+        info("TEST CASE: switches_off_output_check", line_num => 170, file_name => "firing_time_generator_vunit_tb.vhd");
+        info("--------------------------------------------------------------------------------", line_num => 171, file_name => "firing_time_generator_vunit_tb.vhd");
+        test_fire <= (OTHERS => '1');
+        wait until rising_edge(clk);
+        check(fire_u = test_fire, "fire_u equals 1", line_num => 174, file_name => "firing_time_generator_vunit_tb.vhd");
+        check(fire_v = test_fire, "fire_v equals 1", line_num => 175, file_name => "firing_time_generator_vunit_tb.vhd");
+        check(fire_w = test_fire, "fire_w equals 1", line_num => 176, file_name => "firing_time_generator_vunit_tb.vhd");
+        check(fire_time_done = '0', "fire_time_done equals 0", line_num => 177, file_name => "firing_time_generator_vunit_tb.vhd");
+        check(spy_sim_sector = 1, "spy_sim_sector equals 0", line_num => 178, file_name => "firing_time_generator_vunit_tb.vhd");
+        wait for 1 ps; 
+        info("==== TEST CASE FINISHED =====", line_num => 180, file_name => "firing_time_generator_vunit_tb.vhd");
+
+      end if; -- for test_suite
+    end loop test_cases_loop;
+
+    wait for 20 ns;
+    test_runner_cleanup(runner); -- end of simulation 
+  end process test_runner; 
+
+end architecture tb; 
